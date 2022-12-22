06d5524e299f613fd74ee5e7a04fffc104303fba
Sebastian Ynojosa <emilio240104@gmail.com>
2022-12-22 11:27:10 -0400

    La lista ahora tiene dos punteros, uno en la cabeza y otro en el final

--- XOR_linked_list.c
+++ XOR_linked_list.c
@@ -10,42 +10,63 @@ typedef struct nodo{
     struct nodo *ant_sig; /* XOR del nodo siguiente con el anterior */
 }nodo_t;
 
+/* Estructura de la lista */
+typedef struct lista{
+    nodo_t *cabeza, *cola;
+}lista_t;
+
 /* Retorna el valor XOR de las direcciones de los nodos */
 nodo_t* XOR(nodo_t *a, nodo_t *b){
     return (nodo_t*)((uintptr_t)(a)^(uintptr_t)(b));
 }
 
-/* Inserta un nodo al inicio de la lista XOR y lo hace la nueva cabeza de la lista */
-void insert(nodo_t **cabeza_ref, int e){
+// Inicializa ambos apuntadores de la lista en NULL
+void inicializar(lista_t *Lista){
+    Lista->cabeza = NULL;
+    Lista->cola = NULL;
+}
+
+// Retorna 1 si la lista esta vacia o 0 si no lo esta
+int esVacia(lista_t *Lista){
+    if (Lista->cabeza == NULL)
+        return 1;
+    return 0;
+}
+
+// Inserta un nodo al inicio de la lista XOR y lo hace la nueva cabeza de la lista
+void insertarPrincipio(lista_t *Lista, int e){
+    // Guarda memoria para el nuevo nodo
     nodo_t *nuevo = (nodo_t*)malloc(sizeof(nodo_t));
     nuevo->elemento = e;
 
-    /* Como el nuevo nodo se inserta al inicio, ant_sig del nuevo nodo siempre sera XOR de la actual cabeza con NULL */
-    nuevo->ant_sig = XOR(*cabeza_ref,NULL);
+    // Como el nuevo elemento se ingresa al principio, ant_sig del nuevo nodo sera el XOR de la actual cabeza con NULL
+    nuevo->ant_sig = XOR(Lista->cabeza, NULL);
 
-    /* Si la lista no esta vacia, entonces ant_sig de la cabeza actual sera XOR del nuevo nodo y el nodo siguiente a la cabeza */
-    if (*cabeza_ref != NULL){
-        /* (*cabeza_ref)->ant_sig sera XOR de NULL y el siguiente 
-        Entonces si hacemos XOR de ant_sig con NULL, tendremos siguiente */
-        nodo_t *sig = XOR((*cabeza_ref)->ant_sig, NULL);
-        (*cabeza_ref)->ant_sig = XOR(nuevo, sig);
+    // Si la lista esta vacia, entonces se le asigna el nuevo a la cabeza y a la cola
+    if (esVacia(Lista)){
+        Lista->cabeza = nuevo;
+        Lista->cola = nuevo;
+    } // Si la lista no esta vacia, entonces ant_sig de la cabeza actual sera XOR del nuevo nodo y el nodo siguiente a la cabeza
+    else {
+        // Lista->cabeza->ant_sig sera XOR de NULL y el siguiente, entonces si hacemos XOR de ant_sig con NULL, tendremos siguiente
+        nodo_t *sig = XOR(Lista->cabeza->ant_sig, NULL);
+        Lista->cabeza->ant_sig = XOR(nuevo, sig);
+
+        // Cambiamos la cabeza
+        Lista->cabeza = nuevo;
     }
-
-    /* Cambiamos la cabeza */
-    *cabeza_ref = nuevo;
 }
 
-/* Imprime el contenido de la lista hacia adelante */
-void printList(nodo_t *cabeza){
-    nodo_t *actual = cabeza;
+void listarInicioAFinal(lista_t Lista){
+    nodo_t *actual = Lista.cabeza;
     nodo_t *previo = NULL;
     nodo_t *siguiente;
 
     printf("Nodos de la lista doblemente enlazada\n");
 
-    while (actual != NULL){
+    while(actual != NULL){
         // Imprime el nodo
-        printf("%d<-->", actual->elemento);
+        printf("%d ", actual->elemento);
 
         /* Obtiene la direccion del siguiente nodo: actual->ant_sig
         es siguiente^previo, entonces actual->ant_sig^previo sera
@@ -60,18 +81,32 @@ void printList(nodo_t *cabeza){
 
 // Programa de testeo
 int main(void){
-    /* Crea la siguiente lista
-    cabeza-->50<-->40<-->30<-->20<-->10 */
+    lista_t listap;
 
-    nodo_t *cabeza = NULL;
-    insert(&cabeza, 10);
-    insert(&cabeza, 20);
-    insert(&cabeza, 30);
-    insert(&cabeza, 40);
-    insert(&cabeza, 50);
+    inicializar(&listap);
 
-    // Imprime la lista
-    printList(cabeza);
+    if (esVacia(&listap))
+        printf("La lista esta vacia\n");
+    else 
+        printf("La lista NO esta vacia\n");
 
+    insertarPrincipio(&listap, 15);
+    insertarPrincipio(&listap, 10);
+    insertarPrincipio(&listap, 50);
+    insertarPrincipio(&listap, 25);
+    insertarPrincipio(&listap, 30);
+    insertarPrincipio(&listap, 20);
+    insertarPrincipio(&listap, 35);
+    insertarPrincipio(&listap, 45);
+
+
+    if (esVacia(&listap))
+        printf("La lista esta vacia\n");
+    else 
+        printf("La lista NO esta vacia\n");
+
+    listarInicioAFinal(listap);
+
+    
     return 0;
 }
